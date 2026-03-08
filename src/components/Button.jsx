3c30b9d1e9575ba4c29a85b50f823ba986c16a7f
Vladimir <m.a.x.i.m.o.v@yandex.ru>
2026-03-08 14:46:29 +0300

    код для скролла

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,6 +1,24 @@
 const Button = ({ text, className }) => {
 	return (
-		<a className={`${className ?? ""} cta-wrapper`}>
+		<a
+			className={`${className ?? ""} cta-wrapper`}
+			onClick={e => {
+				e.preventDefault();
+				// TODO: сюда добавится прокрутка к следующей секции
+				// const target = document.getElementById("");
+
+				// if (target && id) {
+				// 	const offset = window.innerHeight * 0.15;
+
+				// 	const top =
+				// 		target.getBoundingClientRect().top +
+				// 		window.scrollY -
+				// 		offset;
+
+				// 	window.scrollTo({ top, behavior: "smooth" });
+				// }
+			}}
+		>
 			<div className="cta-button group">
 				<div className="bg-circle" />
 				<p className="text">{text}</p>
