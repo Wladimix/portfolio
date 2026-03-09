52be7b6c29f6af2a60fa3f5c4947918ee25cf5f0
Vladimir <m.a.x.i.m.o.v@yandex.ru>
2026-03-09 22:23:56 +0300

    секция о себе

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import ExperienceSection from "./sections/ExperienceSection";
 import HeroSection from "./sections/HeroSection";
 import LogoSection from "./sections/LogoSection";
 import SkillsSection from "./sections/SkillsSection";
+import ElectricBorderSection from "./sections/ElectricBorderSection/ElectricBorderSection";
 
 const App = () => {
 	return (
@@ -14,6 +15,7 @@ const App = () => {
 			<ExperienceSection />
 			<SkillsSection />
 			<LogoSection />
+			<ElectricBorderSection />
 		</>
 	);
 };
---
+++ src/sections/ElectricBorderSection/ElectricBorderCard.jsx
@@ -0,0 +1,42 @@
+import "./ElectricBorderCard.css";
+
+const ElectricBorderCard = () => {
+	return (
+		<div className="card-container">
+			<div className="inner-container">
+				<div className="border-outer">
+					<div className="main-card">
+						<div className="glow-layer-1"></div>
+						<div className="glow-layer-2"></div>
+						<div className="overlay-1"></div>
+						<div className="overlay-2"></div>
+						<div className="background-glow"></div>
+
+						<div className="content-container">
+							<div className="content-top">
+								<div className="scrollbar-glass">Резюме</div>
+								<p className="title">Обо мне</p>
+							</div>
+
+							<hr className="divider" />
+
+							<div className="content-bottom">
+								<p className="description">
+									Привет! Я ищу работу на позицию разработчика в сфере JavaScript/TypeScript.
+									Мне интересны как backend, так и frontend, так как я успел поработать в обоих
+									направлениях (но для начала я бы хотел развиваться в одном из этих направлений).
+									Также попробовал себя в качестве системного администратора, но всё же решил,
+									что хочу развиваться именно как разработчик. Хочу попасть в коллектив, в котором
+									ценится грамотно написанный и читаемый код. Мне будет интересно при необходимости
+									выполнить тестовое задание и рассказать подробней о себе на собеседовании.
+								</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default ElectricBorderCard;
